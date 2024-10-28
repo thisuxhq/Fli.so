2b78de18819886e316cc6c3d1e20fc31d725a269
SpikeySanju <spikeysanju98@gmail.com>
2024-10-28 23:04:33 +0530

    remove logs

--- src/routes/[slug]/+page.server.ts
+++ src/routes/[slug]/+page.server.ts
@@ -2,7 +2,9 @@ import { error, redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ params, locals }) => {
-  console.log("Loading", params.slug);
+  if (!params.slug) {
+    throw error(400, "Slug is required");
+  }
 
   const url = await locals.pb
     .collection("urls")
@@ -12,15 +14,9 @@ export const load: PageServerLoad = async ({ params, locals }) => {
     throw error(404, "Not found");
   }
 
-  console.log("Got url", url);
-
-  console.log("Updating clicks", url.clicks + 1);
-
   await locals.pb.collection("urls").update(url.id, {
     clicks: url.clicks + 1,
   });
-
-  console.log("Updated clicks", url.clicks + 1);
 
   // redirect to the url
   throw redirect(302, url.url);
